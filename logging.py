0e988842a5782f4f600ee964bd623a38bbb8244f
Ananiya S <102500336+anuananiya16@users.noreply.github.com>
2022-04-04 13:05:54 +0530

    Create logging.py

---
+++ logging.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.debug('This is a debug msg')
+logging.info('This is an info msg')
+logging.warning('This is a warning msg')
+logging.error('This is an error msg')
+logging.critical('This is a critical msg')
